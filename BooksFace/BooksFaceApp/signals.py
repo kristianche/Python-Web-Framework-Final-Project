afe5e271837a2ed0315a2e568c23f8fcff3c824c
kristianche <chelebiev777@gmail.com>
2023-08-16 16:38:53 +0300

    Update!

--- BooksFace/BooksFaceApp/signals.py
+++ BooksFace/BooksFaceApp/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete, pre_delete
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 
